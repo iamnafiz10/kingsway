0ed57018c3c92444a51408b9a4b313bfd6b9d48b
Roshan SaiHam Nafiz <118830871+iamroshansaihamnafiz@users.noreply.github.com>
2024-05-21 16:00:37 +0600

    Up

--- src/app/serviceDetail/page.jsx
+++ src/app/serviceDetail/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {useRouter, useSearchParams} from 'next/navigation';
 import serviceData from "../Data/service-data.json";
 import PageHeroBg from "../../../public/assets/images/page-hero.jpg";
-import Image from "next/image";
+import {Suspense} from 'react'
 
 function ServiceDetail() {
     const router = useRouter();
@@ -20,47 +20,49 @@ function ServiceDetail() {
 
     return (
         <>
-            <section id="page-hero-section" className="relative text-white"
-                     style={{
-                         backgroundImage: `url(${PageHeroBg.src})`,
-                         backgroundSize: 'cover',
-                         backgroundPosition: 'center',
-                         backgroundRepeat: 'no-repeat'
-                     }}
-            >
-                <div className="absolute w-full h-full bg-[#000F21] opacity-80"></div>
-                <div className="relative z-10 flex items-center justify-center">
-                    <div className="container pt-[135px] lg:pt-[90px] h-[250px]">
-                        <h1 className="text-[40px] font-bold">
-                            {title}
-                        </h1>
+            <Suspense>
+                <section id="page-hero-section" className="relative text-white"
+                         style={{
+                             backgroundImage: `url(${PageHeroBg.src})`,
+                             backgroundSize: 'cover',
+                             backgroundPosition: 'center',
+                             backgroundRepeat: 'no-repeat'
+                         }}
+                >
+                    <div className="absolute w-full h-full bg-[#000F21] opacity-80"></div>
+                    <div className="relative z-10 flex items-center justify-center">
+                        <div className="container pt-[135px] lg:pt-[90px] h-[250px]">
+                            <h1 className="text-[40px] font-bold">
+                                {title}
+                            </h1>
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section id="services-section" className="relative bg-white">
-                <div className="container py-[6rem] relative z-10">
-                    <div className="grid grid-cols-1">
-                        <div className="col">
-                            <div className="relative group">
-                                <div className="box bg-white h-full py-6 px-4 rounded">
-                                    {sections && sections.map((section, index) => (
-                                        <div key={index} className="content_wrap pb-4">
-                                            <h2 className="text-[25px] font-bold text-secondary">
-                                                {section.title}
-                                            </h2>
-                                            <p className="mt-2 text-[18px] text-pgcolor pr-6 pb-4">
-                                                {section.description}
-                                            </p>
-                                            <hr/>
-                                        </div>
-                                    ))}
+                <section id="services-section" className="relative bg-white">
+                    <div className="container py-[6rem] relative z-10">
+                        <div className="grid grid-cols-1">
+                            <div className="col">
+                                <div className="relative group">
+                                    <div className="box bg-white h-full py-6 px-4 rounded">
+                                        {sections && sections.map((section, index) => (
+                                            <div key={index} className="content_wrap pb-4">
+                                                <h2 className="text-[25px] font-bold text-secondary">
+                                                    {section.title}
+                                                </h2>
+                                                <p className="mt-2 text-[18px] text-pgcolor pr-6 pb-4">
+                                                    {section.description}
+                                                </p>
+                                                <hr/>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </Suspense>
         </>
     );
 }
